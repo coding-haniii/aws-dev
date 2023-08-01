7064327c58fee55a749efd97cdec7e81894d28c4
coding-haniii <105861729+coding-haniii@users.noreply.github.com>
2023-08-01 18:29:12 +0900

    Create aws-lambda-sns.py

---
+++ cloudformation/aws-lambda-sns.py
@@ -0,0 +1,26 @@
+import json
+import urllib.request
+
+def post_slack(argStr):
+    message = argStr
+    send_data = {
+        "channel": "{SLACK_CHANNEL_NAME}", 
+    	"username": "{user_name|service_name}", 
+    	"text": "real alarm msg send {SLACK_CHANNEL_NAME}."  + argStr + json.dumps(event)
+    }
+    send_text = json.dumps(send_data)
+    request = urllib.request.Request(
+        "https://hooks.slack.com/services/{CUMSTEMED YOUR LINK}", 
+        data=send_text.encode('utf-8'), 
+    )
+
+    with urllib.request.urlopen(request) as response:
+        slack_message = response.read()
+
+def lambda_handler(event, context):
+    post_slack("This alarm generated from AWS Lambda to Slack.")
+
+# add APP "Incoming WebHooks" on your slack
+# smaple test : same curl transaction
+# curl -X POST --data-urlencode "payload={\"channel\": \"#awsAlarm\", \"username\": \"coding-haniii\", \"text\": \"real alarm msg send to slack channel.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/{CUMSTEMED YOUR LINK}
+# icon_emoji : You can change icon in your slack setting
